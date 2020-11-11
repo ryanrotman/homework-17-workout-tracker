31206ddf9a636e76fd678af9155489078308c714
Ryan Rotman <ryan.rotman@gmail.com>
2020-11-10 23:53:32 -0600

    add todo note

--- models/workout.js
+++ models/workout.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    
+    // TODO: add schema code here
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
