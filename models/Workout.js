5a26976bac1cfa0e39bbf700e342746c24aa193b
Ryan Rotman <ryan.rotman@gmail.com>
2020-11-11 12:54:24 -0600

    added new schema based on provided seeds file

---
+++ models/Workout.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    type: {
+        type: String,
+        trim: true,
+        required: "Must include a workout type"
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: "Must include a workout name"
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    weight: {
+        type: Number,
+        required: true
+    },
+    reps: {
+        type: Number,
+        required: true
+    },
+    sets: {
+        type: Number,
+        required: true
+    },
+    distance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
