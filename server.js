fd131f430675a71d97cb610a316c15b8f9279f84
Ryan Rotman <ryan.rotman@gmail.com>
2020-11-11 07:42:09 -0600

    required path and started with get html routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,8 +18,27 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-// TODO: routes will go here?
+// TODO: Routes:
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "./public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "./public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "./public/stats.html"));
+});
+
+app.get("/api/workouts")
+
+app.post("/api/workouts")
+
+app.put("/api/workouts")
+
+app.get("/api/workouts/range")
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
