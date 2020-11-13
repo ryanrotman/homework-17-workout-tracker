ba8de19d8e9504db4098a1e0950fc36bc2c74cd5
Ryan Rotman <ryan.rotman@gmail.com>
2020-11-12 20:38:10 -0600

    created new Workout schema

--- models/Workout.js
+++ models/Workout.js
@@ -3,36 +3,42 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    type: {
-        type: String,
-        trim: true,
-        required: "Must include a workout type"
+    day: {
+        type: Date,
+        default: Date.now
     },
-    name: {
-        type: String,
-        trim: true,
-        required: "Must include a workout name"
-    },
-    duration: {
-        type: Number,
-        required: true
-    },
-    weight: {
-        type: Number,
-        required: true
-    },
-    reps: {
-        type: Number,
-        required: true
-    },
-    sets: {
-        type: Number,
-        required: true
-    },
-    distance: {
-        type: Number,
-        required: true
-    }
+    exercise: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "Must include a workout type"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Must include a workout name"
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        weight: {
+            type: Number,
+            required: true
+        },
+        reps: {
+            type: Number,
+            required: true
+        },
+        sets: {
+            type: Number,
+            required: true
+        },
+        distance: {
+            type: Number,
+            required: true
+        }
+    }]
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
