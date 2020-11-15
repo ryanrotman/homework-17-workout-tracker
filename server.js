06d26a7219a1d25b619045edf2e750818c5ef982
Ryan Rotman <ryan.rotman@gmail.com>
2020-11-15 12:22:50 -0600

    deploying

--- server.js
+++ server.js
@@ -17,7 +17,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 // Routes:
 // FIXME: figure out why this isn't working
